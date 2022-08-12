93adcc7a74485f501a6509b5dff9c043e100d02f
Jennifer Davis <jennifer.davis@dominodatalab.com>
2022-08-12 08:17:19 -0700

    Pushed from Domino: https://prod-field.cs.domino.tech/workspace/jennifer_davis/Yanex-YaML-100b?executionId=62f568e04dd2ae3f7d454cfb

--- examples/.ipynb_checkpoints/generate_unconditional-checkpoint.sh
+++ examples/.ipynb_checkpoints/generate_unconditional-checkpoint.sh
@@ -5,19 +5,22 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 MP_SIZE=8
 
 # Provide path to vocab file and model
-VOCAB_PATH="/mnt/data/Yanex-YaML-100b/vocab/voc_100b.sp"
-MODEL_PATH="/mnt/data/Yanex-YaML-100b/weights/"
+VOCAB_PATH="yalm100b_checkpoint/vocab/voc_100b.sp"
+MODEL_PATH="yalm100b_checkpoint/weights"
 LOAD_ARGS="\
     --vocab-file ${VOCAB_PATH} \
     --load ${MODEL_PATH}"
 
 # Set generation parameters
+UNCOND_ARGS="\
+    --num-samples 2 \
+    --genfile uncond_output.json"
 GEN_ARGS="
     --temperature 1.0 \
     --top_p 0.9 \
     --seed 1234 \
-    --seq-length 256 \
-    --out-seq-length 128"
+    --seq-length 512 \
+    --out-seq-length 512"
 
 HPARAM_ARGS="\
     --pos-encoding-type rotary \
@@ -31,21 +34,21 @@ HPARAM_ARGS="\
     --tokenizer-type SentencePiece \
     --fp16"
 
-#DISTRIBUTED_ARGS="--nproc_per_node $MP_SIZE \
-#                  --nnodes 1 \
-#                  --node_rank 0 \
-#                  --master_addr localhost \
-#                  --master_port=1234"
+DISTRIBUTED_ARGS="--nproc_per_node $MP_SIZE \
+                  --nnodes 1 \
+                  --node_rank 0 \
+                  --master_addr localhost \
+                  --master_port=1234"
 
 COMMON_ARGS="\
-    --num-samples 0 \
     --load-release-checkpoint \
     --batch-size 1 \
     --model-parallel-size $MP_SIZE \
     --make-vocab-size-divisible-by 1"
 
-deepspeed --hostfile /domino/mpi/hosts /mnt/imported/code/YaLM-100B/megatron_lm/tools/generate_samples_gpt2.py \
+torchrun $DISTRIBUTED_ARGS megatron_lm/tools/generate_samples_gpt2.py \
     $LOAD_ARGS \
     $HPARAM_ARGS \
     $COMMON_ARGS \
     $GEN_ARGS \
+    $UNCOND_ARGS
